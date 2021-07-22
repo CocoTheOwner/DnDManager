d5d5cc23d3d4a906d6ac6461ce80221b1b15de6e
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-07-22 16:40:19 +0200

    Move coinweight here, check file exists on load, move default to inv fld

--- Inventory.py
+++ Inventory.py
@@ -1,4 +1,4 @@
-import json, random
+import json, os, traceback
 from Item import Item
 from Coin import Coin
 
@@ -16,7 +16,9 @@ class Inventory:
     # add item to inventory
     # returns true if successful, false if not
     def addItem(self, item: Item):
-        if self.getInventoryWeight() + Coin.getCoinWeight(self.coins) + item.getWeight() * item.getAmount() <= self.max_weight:
+        if self.getInventoryWeight() + self.getCoinWeight() + item.getWeight() * item.getAmount() <= self.max_weight:
+            if item in self.items:
+                item.add(item.amount)
             self.items.append(item)
             return True
         else:
@@ -49,6 +51,7 @@ class Inventory:
         weight = 0
         for item in self.items:
             weight += item.getWeight() * item.getAmount()
+        weight += self.getCoinWeight()
         return weight
 
     # get inventory weight left
@@ -73,6 +76,14 @@ class Inventory:
 
     # load the inventory from a json file
     def load(self, altItems: list, altCoins: dict, altMaxWeight: int):
+        # check that file exists
+        if not os.path.isfile("inventories/" + self.name + '.json'):
+            print("Inventory " + self.name + " does not exist, creating default")
+            # copy inventories/default.json to the inventories folder with the name of the inventory
+            with open("inventories/default.json", 'r') as f:
+                with open("inventories/" + self.name + '.json', 'w') as f2:
+                    f2.write(f.read())
+
         with open("inventories/" + self.name + '.json', 'r') as f:
             data = json.load(f)
             self.__dict__.update(data)
@@ -91,16 +102,84 @@ class Inventory:
                 return False
             return True
 
-    # reset the inventory using the emptyInventory.json file
+    # reset the inventory using the inventories/default.json file
     def reset(self):
-        with open("emptyInventory.json", 'r') as f:
+        with open("inventories/default.json", 'r') as f:
             data = json.load(f)
             name = self.name
             self.__dict__.update(data)
             self.name = name
             self.save()
 
+    # remove the inventory from the inventory folder
+    def remove(self):
+        os.remove("inventories/" + self.name + '.json')
+
+    
+    # condense coins to copper
+    # 1 platinum is 500 copper
+    # 1 gold is 100 copper
+    # 1 electrum is 50 copper
+    # 1 silver is 10 copper
+    # condenses everything to gold coins, silver coins and copper coins
+    # returns none if the coins dictionary is invalid 
+    # (does not contain all PLATINUM, GOLD, ELECTRUM, SILVER, COPPER)
+    def condenseCoins(coins: dict, toPlat: False):
+        
+        if not Coin.isValid(coins):
+            return None
+
+        copper = coins[Coin.COPPER] \
+        + coins[Coin.SILVER]   * 10 \
+        + coins[Coin.ELECTRUM] * 50 \
+        + coins[Coin.GOLD]     * 100\
+        + coins[Coin.PLATINUM] * 500\
+
+        print(copper)
+
+        if toPlat:
+            coins[Coin.PLATINUM] = copper // 500
+            copper = copper % 500
+        else:
+            coins[Coin.PLATINUM] = 0
+        
+        coins[Coin.GOLD]     = copper // 100
+        copper = copper % 100
+        coins[Coin.SILVER]   = copper // 10
+        copper = copper % 10
+        coins[Coin.COPPER]   = copper
+
+        coins[Coin.ELECTRUM] = 0
+        return coins
+        
+    # calculate the total weight of the coins
+    # returns -1 if invalid coins dict
+    def getCoinWeight(self):
+        if not Coin.isValid(self.coins):
+            return -1
+
+        weight = 0
+        for coinType in self.coins:
+            weight += self.coins[coinType] * Coin.weight
+        return weight
+
 # if name is main
 if __name__ == '__main__':
-    inventory = Inventory('test')
-    inventory.reset()
+    # run tests on a copy of the default inventory
+    try:
+        inventory = Inventory('test')
+        inventory.reset()
+        item = Item('Potion', 1, 10)
+        item.addToOverrideDatabase(True, "misc", 0.5)        
+        inventory.addItem(item)
+        inventory.addCoin(Coin('gold', 5))
+        coinWeight = inventory.getCoinWeight()
+        inventory.save()
+        weight = inventory.getInventoryWeight()
+        weightLeft = inventory.getInventoryLeft()
+        item.removeFromDatabase()
+        inventory.remove()
+    except Exception as e:
+        # print a traceback
+        print("Exception raised during inventory test: " + e)
+        traceback.print_exc()
