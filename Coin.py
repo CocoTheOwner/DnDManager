0a3b638d7feb6c93ba1bd252851158c5134a9625
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-07-22 16:32:16 +0200

    Move coin condenser to inventory & add coin checks

--- Coin.py
+++ Coin.py
@@ -14,53 +14,6 @@ class Coin:
 
     weight = 0.02
 
-    # condense coins to copper
-    # 1 platinum is 500 copper
-    # 1 gold is 100 copper
-    # 1 electrum is 50 copper
-    # 1 silver is 10 copper
-    # condenses everything to gold coins, silver coins and copper coins
-    # returns none if the coins dictionary is invalid 
-    # (does not contain all PLATINUM, GOLD, ELECTRUM, SILVER, COPPER)
-    def condenseCoin(coins: dict, toPlat: False):
-        
-        if not Coin.isValid(coins):
-            return None
-
-        copper = coins[Coin.COPPER] \
-        + coins[Coin.SILVER]   * 10 \
-        + coins[Coin.ELECTRUM] * 50 \
-        + coins[Coin.GOLD]     * 100\
-        + coins[Coin.PLATINUM] * 500\
-
-        print(copper)
-
-        if toPlat:
-            coins[Coin.PLATINUM] = copper // 500
-            copper = copper % 500
-        else:
-            coins[Coin.PLATINUM] = 0
-        
-        coins[Coin.GOLD]     = copper // 100
-        copper = copper % 100
-        coins[Coin.SILVER]   = copper // 10
-        copper = copper % 10
-        coins[Coin.COPPER]   = copper
-
-        coins[Coin.ELECTRUM] = 0
-        return coins
-        
-    # calculate the total weight of the coins
-    # returns -1 if invalid coins dict
-    def getCoinWeight(coins: dict):
-        if not Coin.isValid(coins):
-            return -1
-
-        weight = 0
-        for coinType in coins:
-            weight += coins[coinType] * Coin.weight
-        return weight
-
     # check if a coin dictionary is valid
     def isValid(coins: dict):
         for coinType in Coin.EMPTY:
@@ -76,3 +29,8 @@ class Coin:
 
         coins[self.type] += self.amount
         return True
+
+# if name is main test the class
+if __name__ == "__main__":
+    c = Coin(Coin.PLATINUM, 1)
+    print(c.getCoinWeight(Coin.EMPTY))
